3c9d94828fa73cf50d219aa91ed97badf2a7aea4
man-ware <nadnacilla2021@plm.edu.ph>
2024-07-02 05:36:55 +0800

    pang pagulong file tinaggal

--- CIA-1-LPG-kopya/database/show-users.php
+++
@@ -1,9 +0,0 @@
-<?php 
-     include('connection.php');
-
-    $stmt = $conn->prepare("SELECT * FROM users ORDER BY created_at DESC");
-    $stmt->execute();
-    $stmt->setFEtchMode(PDO::FETCH_ASSOC);
-
-    return $stmt->fetchAll();
-    
